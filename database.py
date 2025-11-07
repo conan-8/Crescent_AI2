b35bc691fbd2a83d0aebeb70c17e262ec52e9de0
Hongyi Sun <37144566+Kevin09sun@users.noreply.github.com>
2025-11-07 10:46:43 -0500

    some urls

---
+++ database.py
@@ -0,0 +1,18 @@
+import chromadb
+from crawl4ai import *
+
+urls = ["https://www.crescentschool.org/family-handbook",
+        "https://www.crescentschool.org/family-handbook/general-information",
+        "https://www.crescentschool.org/family-handbook/general-information/a-message-from-our-headmaster",
+        "https://www.crescentschool.org/family-handbook/general-information/mission-and-values",
+        "https://www.crescentschool.org/family-handbook/general-information/campus-access",
+        "https://www.crescentschool.org/family-handbook/general-information/transportation-and-parking",
+        "https://www.crescentschool.org/family-handbook/general-information/food-services",
+        "https://www.crescentschool.org/family-handbook/general-information/school-communication",
+        "https://www.crescentschool.org/family-handbook/general-information/dress-code",
+        "https://www.crescentschool.org/family-handbook/general-information/student-property",
+        "https://www.crescentschool.org/family-handbook/general-information/house-system",
+        "https://www.crescentschool.org/family-handbook/general-information/prefect-system",
+        "https://www.crescentschool.org/family-handbook/general-information/school-constitution"
+]
+
